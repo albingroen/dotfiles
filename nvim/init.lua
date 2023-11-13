87ab0fbf2e20fe72560656d76b070e4cfb69be49
Albin Groen <albin.groen@gmail.com>
2023-11-13 17:00:56 +0100

    Overhaul
    Kitty
    - Change colorscheme to kanagawa
    - Remove python scripts for knubie/vim-kitty-navigator
    
    Neovim
    - Change colorscheme to kanagawa
    - Remove indent guides
    - Use native LSP instead of CoC
    - Remove knubie/vim-kitty-navigator plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -13,21 +13,21 @@ vim.o.tabstop = 2
 vim.o.shiftwidth = 2
 vim.o.softtabstop = 2
 vim.o.expandtab = true
-vim.o.smartindent = true
 vim.o.termguicolors = true
 vim.o.hidden = true
 vim.o.backup = false
 vim.o.writebackup = false
 vim.o.swapfile = false
-vim.o.updatetime = 300
 vim.o.cmdheight = 0
 vim.opt.list = true
 vim.o.laststatus = 3
-vim.opt.listchars = {leadmultispace = "│ ", multispace = "│ ", tab = "│ "}
+vim.opt.statusline = " %f %m %= %l:%c ♥ "
+vim.o.splitright = true
+vim.o.splitbelow = 3
+vim.o.signcolumn = "yes"
 
 local map = vim.keymap.set
 
-map("n", "<leader>c", ":CocRestart<CR><CR>", {noremap = true, silent = true, desc = "Restart CoC language servers"})
 map("n", "<C-H>", "<C-W><C-H>", {noremap = true})
 map("n", "<C-J>", "<C-W><C-J>", {noremap = true})
 map("n", "<C-K>", "<C-W><C-K>", {noremap = true})
@@ -45,82 +45,183 @@ map("n", "<leader>k", ":cprevious<CR>", {noremap = true, silent = true})
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({"git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath})
+  vim.fn.system(
+    {"git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath}
+  )
 end
 
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup(
   {
-    "knubie/vim-kitty-navigator",
-    {
-      "nvim-treesitter/nvim-treesitter",
-      config = function()
-        require "nvim-treesitter.configs".setup {
-          ensure_installed = {"typescript"},
-          highlight = { enable = true },
-          indent = { enable = true }
-        }
-      end
-    },
-    {
-      "JoosepAlviste/nvim-ts-context-commentstring",
-      event = "VeryLazy"
-    },
-    {
-      "numToStr/Comment.nvim",
-      lazy = false
-    },
-    {"neoclide/coc.nvim", branch = "release"},
-    {
-      "kvrohit/rasmus.nvim",
-      lazy = false,
-      priority = 1000,
-      config = function()
-        vim.cmd([[colorscheme rasmus]])
-      end
-    },
-    {
-      "nvim-telescope/telescope.nvim",
-      dependencies = {{"nvim-lua/plenary.nvim"}},
-      event = "VeryLazy"
-    },
-    {
-      "kylechui/nvim-surround",
-      event = "VeryLazy",
-      config = function()
-        require("nvim-surround").setup()
-      end
-    },
-  }
-)
-
-local telescope_builtin = require("telescope.builtin")
-
-map("n", "<leader>.", "<Plug>(coc-codeaction)", {desc = "Code actions"})
-map("n", "gd", "<Plug>(coc-definition)", {silent = true})
-map("n", "gr", "<Plug>(coc-references)", {silent = true})
-map("n", "K", ":call CocActionAsync('doHover')<CR>", {silent = true, noremap = true})
-map("n", "<leader>rn", "<Plug>(coc-rename)", {})
-map("n", "<leader>f", ":CocCommand prettier.formatFile<CR>", {noremap = true, desc = "Format file"})
-map("i", "<C-Space>", "coc#refresh()", {silent = true, expr = true})
-map("i", "<TAB>", "coc#pum#visible() ? coc#pum#next(1) : '<TAB>'", {noremap = true, silent = true, expr = true})
-map("i", "<S-TAB>", "coc#pum#visible() ? coc#pum#prev(1) : '<C-h>'", {noremap = true, expr = true})
-map( "i", "<CR>", "coc#pum#visible() ? coc#pum#confirm() : '<C-G>u<CR><C-R>=coc#on_enter()<CR>'", {silent = true, expr = true, noremap = true})
-map("n", "<C-P>", telescope_builtin.find_files, {})
-map("n", "<C-F>", telescope_builtin.live_grep, {})
-
-vim.api.nvim_create_autocmd(
-  "VimEnter",
-  {
-    callback = function()
-      if vim.fn.argv(0) == "" then
-        require("telescope.builtin").find_files()
-      end
-    end
+    "L3MON4D3/LuaSnip",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/nvim-cmp",
+    "saadparwaiz1/cmp_luasnip",
+    "lewis6991/gitsigns.nvim",
+    "neovim/nvim-lspconfig",
+    "nvim-tree/nvim-web-devicons",
+    "nvim-treesitter/nvim-treesitter",
+    "stevearc/oil.nvim",
+    {"JoosepAlviste/nvim-ts-context-commentstring", event = "VeryLazy"},
+    {"kylechui/nvim-surround", event = "VeryLazy"},
+    {"numToStr/Comment.nvim", lazy = false},
+    {"nvim-telescope/telescope.nvim", dependencies = {{"nvim-lua/plenary.nvim"}}, event = "VeryLazy"},
+    {"rebelot/kanagawa.nvim", lazy = false, priority = 1000},
+    {"windwp/nvim-autopairs", event = "InsertEnter", opts = {}}
   }
 )
 
 require("Comment").setup {
   pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook()
 }
+
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+local lspconfig = require("lspconfig")
+
+local servers = {"tsserver"}
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    capabilities = capabilities
+  }
+end
+
+local luasnip = require "luasnip"
+
+local cmp = require "cmp"
+cmp.setup {
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end
+  },
+  mapping = cmp.mapping.preset.insert(
+    {
+      ["<C-Space>"] = cmp.mapping.complete(),
+      ["<CR>"] = cmp.mapping.confirm {
+        behavior = cmp.ConfirmBehavior.Replace,
+        select = true
+      },
+      ["<Tab>"] = cmp.mapping(
+        function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          else
+            fallback()
+          end
+        end,
+        {"i", "s"}
+      ),
+      ["<S-Tab>"] = cmp.mapping(
+        function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end,
+        {"i", "s"}
+      )
+    }
+  ),
+  sources = {
+    {name = "nvim_lsp"},
+    {name = "luasnip"},
+    {name = "buffer"}
+  }
+}
+
+map("n", "<space>e", vim.diagnostic.open_float)
+map("n", "[d", vim.diagnostic.goto_prev)
+map("n", "]d", vim.diagnostic.goto_next)
+
+vim.api.nvim_create_autocmd(
+  "LspAttach",
+  {
+    group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+    callback = function(ev)
+      local opts = {buffer = ev.buf}
+      map("n", "gd", vim.lsp.buf.definition, opts)
+      map("n", "K", vim.lsp.buf.hover, opts)
+      map("n", "<space>rn", vim.lsp.buf.rename, opts)
+      map({"n", "v"}, "<space>.", vim.lsp.buf.code_action, opts)
+      map("n", "gr", vim.lsp.buf.references, opts)
+      map(
+        "n",
+        "<space>f",
+        function()
+          vim.lsp.buf.format {async = true}
+        end,
+        opts
+      )
+    end
+  }
+)
+
+-- Git signs
+require("gitsigns").setup {
+  signs = {add = {text = "+"}},
+  current_line_blame = true,
+  numhl = true
+}
+
+-- Oil.nvim
+require("oil").setup()
+
+map("n", "-", "<CMD>Oil<CR>", {desc = "Open parent directory"})
+
+-- Treesitter
+require "nvim-treesitter.configs".setup {
+  ensure_installed = {"typescript"},
+  highlight = {enable = true},
+  indent = {enable = true}
+}
+
+-- Colorscheme
+require("kanagawa").setup(
+  {
+    overrides = function(colors)
+      local theme = colors.theme
+      return {
+        TelescopeTitle = {fg = theme.ui.special, bold = true},
+        TelescopePromptNormal = {bg = theme.ui.bg_p1},
+        TelescopePromptBorder = {fg = theme.ui.bg_p1, bg = theme.ui.bg_p1},
+        TelescopeResultsNormal = {fg = theme.ui.fg_dim, bg = theme.ui.bg_m1},
+        TelescopeResultsBorder = {fg = theme.ui.bg_m1, bg = theme.ui.bg_m1},
+        TelescopePreviewNormal = {bg = theme.ui.bg_dim},
+        TelescopePreviewBorder = {bg = theme.ui.bg_dim, fg = theme.ui.bg_dim},
+        Pmenu = {fg = theme.ui.shade0, bg = theme.ui.bg_p1},
+        PmenuSel = {fg = "NONE", bg = theme.ui.bg_p3},
+        PmenuSbar = {bg = theme.ui.bg_m1},
+        PmenuThumb = {bg = theme.ui.bg_p2}
+      }
+    end
+  }
+)
+
+vim.opt.background = "dark"
+vim.cmd.colorscheme "kanagawa-dragon"
+
+-- Telescope
+local telescope = require("telescope")
+local telescope_builtin = require("telescope.builtin")
+
+telescope.setup {
+  defaults = {
+    prompt_prefix = " ",
+    selection_caret = " ",
+    entry_prefix = " "
+  }
+}
+
+map("n", "<C-P>", telescope_builtin.find_files, {})
+map("n", "<C-F>", telescope_builtin.live_grep, {})
+
+-- nvim-surround
+require("nvim-surround").setup()
