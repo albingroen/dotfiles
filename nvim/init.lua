28f021e059beea0cc9c869cf3665a7f9c833b26e
Albin Groen <albin.groen@gmail.com>
2023-11-15 10:31:32 +0100

    feat(nvim): add native LSP feature

--- nvim/init.lua
+++ nvim/init.lua
@@ -19,108 +19,211 @@ vim.o.hidden = true
 vim.o.backup = false
 vim.o.writebackup = false
 vim.o.swapfile = false
-vim.o.updatetime = 300
 vim.o.cmdheight = 0
+vim.o.signcolumn = "yes"
+vim.o.splitright = true
+vim.o.splitbelow = true
 vim.opt.list = true
 vim.o.laststatus = 3
-vim.opt.listchars = {leadmultispace = "│ ", multispace = "│ ", tab = "│ "}
+vim.opt.listchars = { leadmultispace = "│ ", multispace = "│ ", tab = "│ " }
 
 local map = vim.keymap.set
 
-map("n", "<leader>c", ":CocRestart<CR><CR>", {noremap = true, silent = true, desc = "Restart CoC language servers"})
-map("n", "<C-H>", "<C-W><C-H>", {noremap = true})
-map("n", "<C-J>", "<C-W><C-J>", {noremap = true})
-map("n", "<C-K>", "<C-W><C-K>", {noremap = true})
-map("n", "<C-L>", "<C-W><C-L>", {noremap = true})
-map("n", "<C-S>", ":%s/", {noremap = true})
-map("n", "sp", ":sp<CR>", {noremap = true})
-map("n", "tj", ":tabprev<CR>", {noremap = true})
-map("n", "tk", ":tabnext<CR>", {noremap = true})
-map("n", "tn", ":tabnew<CR>", {noremap = true})
-map("n", "to", ":tabo<CR>", {noremap = true})
-map("n", "vs", ":vs<CR>", {noremap = true})
-map("n", "<leader>j", ":cnext<CR>", {noremap = true, silent = true})
-map("n", "<leader>k", ":cprevious<CR>", {noremap = true, silent = true})
+map("n", "<C-H>", "<C-W><C-H>", { noremap = true })
+map("n", "<C-J>", "<C-W><C-J>", { noremap = true })
+map("n", "<C-K>", "<C-W><C-K>", { noremap = true })
+map("n", "<C-L>", "<C-W><C-L>", { noremap = true })
+map("n", "<C-S>", ":%s/", { noremap = true })
+map("n", "sp", ":sp<CR>", { noremap = true })
+map("n", "tj", ":tabprev<CR>", { noremap = true })
+map("n", "tk", ":tabnext<CR>", { noremap = true })
+map("n", "tn", ":tabnew<CR>", { noremap = true })
+map("n", "to", ":tabo<CR>", { noremap = true })
+map("n", "vs", ":vs<CR>", { noremap = true })
+map("n", "<leader>j", ":cnext<CR>", { noremap = true, silent = true })
+map("n", "<leader>k", ":cprevious<CR>", { noremap = true, silent = true })
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({"git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath})
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup(
-  {
-    "knubie/vim-kitty-navigator",
-    {
-      "nvim-treesitter/nvim-treesitter",
-      config = function()
-        require "nvim-treesitter.configs".setup {
-          ensure_installed = {"typescript"},
-          highlight = { enable = true },
-          indent = { enable = true }
-        }
-      end
-    },
-    {
-      "JoosepAlviste/nvim-ts-context-commentstring",
-      event = "VeryLazy"
-    },
-    {
-      "numToStr/Comment.nvim",
-      lazy = false
-    },
-    {"neoclide/coc.nvim", branch = "release"},
-    {
-      "kvrohit/rasmus.nvim",
-      lazy = false,
-      priority = 1000,
-      config = function()
-        vim.cmd([[colorscheme rasmus]])
-      end
-    },
-    {
-      "nvim-telescope/telescope.nvim",
-      dependencies = {{"nvim-lua/plenary.nvim"}},
-      event = "VeryLazy"
-    },
-    {
-      "kylechui/nvim-surround",
-      event = "VeryLazy",
-      config = function()
-        require("nvim-surround").setup()
-      end
-    },
-  }
-)
+require("lazy").setup({
+	"williamboman/mason.nvim",
+	"williamboman/mason-lspconfig.nvim",
+	"neovim/nvim-lspconfig",
+	"hrsh7th/nvim-cmp",
+	"hrsh7th/cmp-nvim-lsp",
+	"saadparwaiz1/cmp_luasnip",
+	"L3MON4D3/LuaSnip",
+	"hrsh7th/cmp-buffer",
+	"hrsh7th/cmp-path",
+	"knubie/vim-kitty-navigator",
+	{
+		"stevearc/conform.nvim",
+		event = { "BufWritePre" },
+		cmd = { "ConformInfo" },
+		keys = {
+			{
+				"<leader>f",
+				function()
+					require("conform").format({ timeout_ms = 500, lsp_fallback = true })
+				end,
+				mode = "",
+				desc = "Format buffer",
+			},
+		},
+		opts = {
+			formatters_by_ft = {
+				lua = { "stylua" },
+				typescript = { { "prettierd" } },
+				typescriptreact = { { "prettierd" } },
+			},
+			format_on_save = { timeout_ms = 500, lsp_fallback = true },
+		},
+	},
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		opts = {},
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = { "typescript" },
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+	},
+	{
+		"JoosepAlviste/nvim-ts-context-commentstring",
+		event = "VeryLazy",
+	},
+	{
+		"numToStr/Comment.nvim",
+		lazy = false,
+	},
+	{
+		"kvrohit/rasmus.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.cmd([[colorscheme rasmus]])
+		end,
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = { { "nvim-lua/plenary.nvim" } },
+		event = "VeryLazy",
+	},
+	{
+		"kylechui/nvim-surround",
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup()
+		end,
+	},
+})
 
 local telescope_builtin = require("telescope.builtin")
 
-map("n", "<leader>.", "<Plug>(coc-codeaction)", {desc = "Code actions"})
-map("n", "gd", "<Plug>(coc-definition)", {silent = true})
-map("n", "gr", "<Plug>(coc-references)", {silent = true})
-map("n", "K", ":call CocActionAsync('doHover')<CR>", {silent = true, noremap = true})
-map("n", "<leader>rn", "<Plug>(coc-rename)", {})
-map("n", "<leader>f", ":CocCommand prettier.formatFile<CR>", {noremap = true, desc = "Format file"})
-map("i", "<C-Space>", "coc#refresh()", {silent = true, expr = true})
-map("i", "<TAB>", "coc#pum#visible() ? coc#pum#next(1) : '<TAB>'", {noremap = true, silent = true, expr = true})
-map("i", "<S-TAB>", "coc#pum#visible() ? coc#pum#prev(1) : '<C-h>'", {noremap = true, expr = true})
-map( "i", "<CR>", "coc#pum#visible() ? coc#pum#confirm() : '<C-G>u<CR><C-R>=coc#on_enter()<CR>'", {silent = true, expr = true, noremap = true})
 map("n", "<C-P>", telescope_builtin.find_files, {})
 map("n", "<C-F>", telescope_builtin.live_grep, {})
 
-vim.api.nvim_create_autocmd(
-  "VimEnter",
-  {
-    callback = function()
-      if vim.fn.argv(0) == "" then
-        require("telescope.builtin").find_files()
-      end
-    end
-  }
-)
+vim.api.nvim_create_autocmd("VimEnter", {
+	callback = function()
+		if vim.fn.argv(0) == "" then
+			require("telescope.builtin").find_files()
+		end
+	end,
+})
 
-require("Comment").setup {
-  pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook()
-}
+require("Comment").setup({
+	pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+})
+
+-- LSP
+require("mason").setup()
+require("mason-lspconfig").setup()
+
+vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
+
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+	callback = function(ev)
+		local opts = { buffer = ev.buf }
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+		vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
+		vim.keymap.set({ "n", "v" }, "<space>.", vim.lsp.buf.code_action, opts)
+		vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+	end,
+})
+
+-- Completion
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+require("mason-lspconfig").setup_handlers({
+	function(server_name)
+		require("lspconfig")[server_name].setup({
+			capabilities = capabilities,
+		})
+	end,
+})
+
+local luasnip = require("luasnip")
+local cmp = require("cmp")
+
+cmp.setup({
+	snippet = {
+		expand = function(args)
+			luasnip.lsp_expand(args.body)
+		end,
+	},
+	mapping = cmp.mapping.preset.insert({
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<CR>"] = cmp.mapping.confirm({
+			behavior = cmp.ConfirmBehavior.Replace,
+			select = true,
+		}),
+		["<Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+		["<S-Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif luasnip.jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+	}),
+	sources = {
+		{ name = "nvim_lsp", max_item_count = 10 },
+		{ name = "luasnip", max_item_count = 5 },
+		{ name = "buffer", max_item_count = 5 },
+		{ name = "path" },
+	},
+})
