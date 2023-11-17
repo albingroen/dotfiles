13e813f5c97e0b99c294e4a24c5eed9a023597e2
Albin Groen <albin.groen@gmail.com>
2023-11-17 09:09:44 +0100

    refactor: use dropdown theme for nvim find_files command

--- nvim/init.lua
+++ nvim/init.lua
@@ -128,3 +128,12 @@ vim.api.nvim_create_autocmd(
 require("Comment").setup {
   pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook()
 }
+
+require('telescope').setup{
+  pickers = {
+    find_files = {
+      theme = "dropdown",
+      previewer = false
+    }
+  }
+}
