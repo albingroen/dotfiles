623fcbfeb1eb128141d2201ddb1da64f688a7cc3
Albin Groen <albin.groen@gmail.com>
2023-12-01 11:19:04 +0100

    refactor: restructure nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -23,22 +23,19 @@ vim.opt.list = true
 vim.o.laststatus = 3
 vim.o.pumheight = 10
 
-local map = vim.keymap.set
-
-map("n", "<leader>c", ":CocRestart<CR><CR>", {noremap = true, silent = true, desc = "Restart CoC language servers"})
-map("n", "<C-H>", "<C-W><C-H>", {noremap = true})
-map("n", "<C-J>", "<C-W><C-J>", {noremap = true})
-map("n", "<C-K>", "<C-W><C-K>", {noremap = true})
-map("n", "<C-L>", "<C-W><C-L>", {noremap = true})
-map("n", "<C-S>", ":%s/", {noremap = true})
-map("n", "sp", ":sp<CR>", {noremap = true})
-map("n", "tj", ":tabprev<CR>", {noremap = true})
-map("n", "tk", ":tabnext<CR>", {noremap = true})
-map("n", "tn", ":tabnew<CR>", {noremap = true})
-map("n", "to", ":tabo<CR>", {noremap = true})
-map("n", "vs", ":vs<CR>", {noremap = true})
-map("n", "<leader>j", ":cnext<CR>", {noremap = true, silent = true})
-map("n", "<leader>k", ":cprevious<CR>", {noremap = true, silent = true})
+vim.keymap.set("n", "<C-H>", "<C-W><C-H>", {noremap = true})
+vim.keymap.set("n", "<C-J>", "<C-W><C-J>", {noremap = true})
+vim.keymap.set("n", "<C-K>", "<C-W><C-K>", {noremap = true})
+vim.keymap.set("n", "<C-L>", "<C-W><C-L>", {noremap = true})
+vim.keymap.set("n", "<C-S>", ":%s/", {noremap = true})
+vim.keymap.set("n", "sp", ":sp<CR>", {noremap = true})
+vim.keymap.set("n", "tj", ":tabprev<CR>", {noremap = true})
+vim.keymap.set("n", "tk", ":tabnext<CR>", {noremap = true})
+vim.keymap.set("n", "tn", ":tabnew<CR>", {noremap = true})
+vim.keymap.set("n", "to", ":tabo<CR>", {noremap = true})
+vim.keymap.set("n", "vs", ":vs<CR>", {noremap = true})
+vim.keymap.set("n", "<leader>j", ":cnext<CR>", {noremap = true, silent = true})
+vim.keymap.set("n", "<leader>k", ":cprevious<CR>", {noremap = true, silent = true})
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
@@ -58,9 +55,15 @@ require("lazy").setup(
         "tpope/vim-rhubarb",
         "tpope/vim-fugitive",
         {"JoosepAlviste/nvim-ts-context-commentstring", event = "VeryLazy"},
-        {"nvim-telescope/telescope.nvim", event = "VeryLazy"},
-        {"neoclide/coc.nvim", branch = "release"},
         {"numToStr/Comment.nvim", lazy = false},
+        {
+            "nvim-telescope/telescope.nvim",
+            event = "VeryLazy",
+            keys = {
+                {"<C-p>", "<cmd>Telescope find_files<cr>", desc = "Find files"},
+                {"<C-f>", "<cmd>Telescope live_grep<cr>", desc = "Live grep"},
+            }
+        },
         {
             "nvim-treesitter/nvim-treesitter",
             build = ":TSUpdate",
@@ -87,36 +90,38 @@ require("lazy").setup(
             config = function()
                 require("nvim-surround").setup()
             end
-        }
+        },
+        {
+            "neoclide/coc.nvim",
+            branch = "release",
+            config = function()
+                vim.keymap.set("n", "<leader>c", ":CocRestart<CR><CR>", {noremap = true, silent = true, desc = "Restart CoC language servers"})
+                vim.keymap.set("n", "<leader>.", "<Plug>(coc-codeaction)", {desc = "Code actions"})
+                vim.keymap.set("n", "gd", "<Plug>(coc-definition)", {silent = true})
+                vim.keymap.set("n", "gr", "<Plug>(coc-references)", {silent = true})
+                vim.keymap.set("n", "K", ":call CocActionAsync('doHover')<CR>", {silent = true, noremap = true})
+                vim.keymap.set("n", "<leader>rn", "<Plug>(coc-rename)", {})
+                vim.keymap.set("n", "<leader>f", ":CocCommand prettier.formatFile<CR>", {noremap = true, desc = "Format file"})
+                vim.keymap.set("i", "<C-Space>", "coc#refresh()", {silent = true, expr = true})
+                vim.keymap.set("i", "<TAB>", "coc#pum#visible() ? coc#pum#next(1) : '<TAB>'", {noremap = true, silent = true, expr = true})
+                vim.keymap.set("i", "<S-TAB>", "coc#pum#visible() ? coc#pum#prev(1) : '<C-h>'", {noremap = true, expr = true})
+                vim.keymap.set(
+                    "i",
+                    "<CR>",
+                    "coc#pum#visible() ? coc#pum#confirm() : '<C-G>u<CR><C-R>=coc#on_enter()<CR>'",
+                    {silent = true, expr = true, noremap = true}
+                )
+            end
+        },
     }
 )
-
-local telescope_builtin = require("telescope.builtin")
-
-map("n", "<leader>.", "<Plug>(coc-codeaction)", {desc = "Code actions"})
-map("n", "gd", "<Plug>(coc-definition)", {silent = true})
-map("n", "gr", "<Plug>(coc-references)", {silent = true})
-map("n", "K", ":call CocActionAsync('doHover')<CR>", {silent = true, noremap = true})
-map("n", "<leader>rn", "<Plug>(coc-rename)", {})
-map("n", "<leader>f", ":CocCommand prettier.formatFile<CR>", {noremap = true, desc = "Format file"})
-map("i", "<C-Space>", "coc#refresh()", {silent = true, expr = true})
-map("i", "<TAB>", "coc#pum#visible() ? coc#pum#next(1) : '<TAB>'", {noremap = true, silent = true, expr = true})
-map("i", "<S-TAB>", "coc#pum#visible() ? coc#pum#prev(1) : '<C-h>'", {noremap = true, expr = true})
-map(
-    "i",
-    "<CR>",
-    "coc#pum#visible() ? coc#pum#confirm() : '<C-G>u<CR><C-R>=coc#on_enter()<CR>'",
-    {silent = true, expr = true, noremap = true}
-)
-map("n", "<C-P>", telescope_builtin.find_files, {})
-map("n", "<C-F>", telescope_builtin.live_grep, {})
 
 vim.api.nvim_create_autocmd(
     "VimEnter",
     {
         callback = function()
             if vim.fn.argv(0) == "" then
-                telescope_builtin.find_files()
+                require("telescope.builtin").find_files()
             end
         end
     }
