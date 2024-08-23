ec264a92a2c76b76e2e34aba7d4812534bcd14ea
Albin Groen <albin.groen@gmail.com>
2024-08-23 18:19:40 +0200

    feat: set up nvim integration for wezterm + small stuff

--- .wezterm.lua
+++ .wezterm.lua
@@ -3,64 +3,55 @@ local config = {}
 
 local act = wezterm.action
 
-config.color_scheme = "GruvboxDarkHard"
-config.font_size = 14
-config.line_height = 1.2
-config.tab_bar_at_bottom = true
-config.window_padding = {
-	left = 0,
-	right = 0,
-	top = 0,
-	bottom = 0,
-}
-
-config.inactive_pane_hsb = {
-	saturation = 0.8,
-	brightness = 0.6,
-}
-
-config.window_close_confirmation = "NeverPrompt"
-
-config.keys = {
-	-- This will create a new split and run your default program inside it
-	{
-		key = "Enter",
-		mods = "CMD",
-		action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-	},
-	{
-		key = "Backspace",
-		mods = "CMD",
-		action = act.SplitVertical({ domain = "CurrentPaneDomain" }),
-	},
-	{
-		key = "d",
-		mods = "CMD|SHIFT",
-		action = act.CloseCurrentPane({ confirm = true }),
-	},
-	{
-		key = "h",
-		mods = "CTRL|SHIFT",
-		action = act.ActivatePaneDirection("Left"),
-	},
-	{
-		key = "l",
-		mods = "CTRL|SHIFT",
-		action = act.ActivatePaneDirection("Right"),
-	},
-	{
-		key = "k",
-		mods = "CTRL|SHIFT",
-		action = act.ActivatePaneDirection("Up"),
-	},
-	{
-		key = "j",
-		mods = "CTRL|SHIFT",
-		action = act.ActivatePaneDirection("Down"),
-	},
-}
-
+-- Basics
+config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
+config.inactive_pane_hsb = { saturation = 0.8, brightness = 0.6 }
 config.send_composed_key_when_left_alt_is_pressed = true
 config.send_composed_key_when_right_alt_is_pressed = true
+config.window_close_confirmation = "NeverPrompt"
+config.colors = { scrollbar_thumb = "#444444" }
+config.color_scheme = "GruvboxDarkHard"
+config.enable_scroll_bar = true
+config.tab_bar_at_bottom = true
+config.line_height = 1.2
+config.font_size = 14
+
+-- START: Handle moving between Wezterm panes and Neovim instances
+local function isViProcess(pane)
+	return pane:get_foreground_process_name():find("n?vim") ~= nil or pane:get_title():find("n?vim") ~= nil
+end
+
+local function conditionalActivatePane(window, pane, pane_direction, vim_direction)
+	if isViProcess(pane) then
+		window:perform_action(act.SendKey({ key = vim_direction, mods = "CTRL" }), pane)
+		window:perform_action(act.ActivatePaneDirection(pane_direction), pane)
+	end
+end
+
+wezterm.on("ActivatePaneDirection-right", function(window, pane)
+	conditionalActivatePane(window, pane, "Right", "l")
+end)
+wezterm.on("ActivatePaneDirection-left", function(window, pane)
+	conditionalActivatePane(window, pane, "Left", "h")
+end)
+wezterm.on("ActivatePaneDirection-up", function(window, pane)
+	conditionalActivatePane(window, pane, "Up", "k")
+end)
+wezterm.on("ActivatePaneDirection-down", function(window, pane)
+	conditionalActivatePane(window, pane, "Down", "j")
+end)
+
+config.keys = {
+	{ key = "h", mods = "CTRL", action = act.EmitEvent("ActivatePaneDirection-left") },
+	{ key = "j", mods = "CTRL", action = act.EmitEvent("ActivatePaneDirection-down") },
+	{ key = "k", mods = "CTRL", action = act.EmitEvent("ActivatePaneDirection-up") },
+	{ key = "l", mods = "CTRL", action = act.EmitEvent("ActivatePaneDirection-right") },
+	-- END: Handle moving between Wezterm panes and Neovim instances
+
+	-- Spawn and kill panes
+	{ key = "Enter", mods = "CMD", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+	{ key = "Backspace", mods = "CMD", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+	{ key = "d", mods = "CMD|SHIFT", action = act.CloseCurrentPane({ confirm = true }) },
+}
 
 return config
