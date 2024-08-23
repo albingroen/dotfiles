e5941d5c14e24c74bc19b20697a2d27d978fe95a
Albin Groen <albin.groen@gmail.com>
2024-08-23 18:19:22 +0200

    feat: set up wezterm integration for nvim

--- nvim/lua/plugins/gruvbox.lua
+++ nvim/lua/plugins/gruvbox.lua
@@ -7,6 +7,7 @@ return {
 			overrides = {
 				SignColumn = { bg = "None" },
 			},
+			dim_inactive = true,
 		})
 
 		vim.cmd("colorscheme gruvbox")
---
+++ nvim/lua/plugins/navigator.lua
@@ -0,0 +1,12 @@
+return {
+	"numToStr/Navigator.nvim",
+	config = function()
+		require("Navigator").setup()
+
+		vim.keymap.set({ "n", "t" }, "<C-h>", "<CMD>NavigatorLeft<CR>")
+		vim.keymap.set({ "n", "t" }, "<C-l>", "<CMD>NavigatorRight<CR>")
+		vim.keymap.set({ "n", "t" }, "<C-k>", "<CMD>NavigatorUp<CR>")
+		vim.keymap.set({ "n", "t" }, "<C-j>", "<CMD>NavigatorDown<CR>")
+		vim.keymap.set({ "n", "t" }, "<C-p>", "<CMD>NavigatorPrevious<CR>")
+	end,
+}
