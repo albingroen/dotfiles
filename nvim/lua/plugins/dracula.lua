a89898470f452aeec6b2b39eae66a427b51b59ad
Albin Groen <albin.groen@gmail.com>
2025-01-29 09:04:59 +0100

    update config again

--- nvim/lua/plugins/dracula.lua
+++ nvim/lua/plugins/dracula.lua
@@ -1,11 +1,11 @@
 return {
 	"ellisonleao/gruvbox.nvim",
+	lazy = false,
 	priority = 1000,
 	config = function()
 		require("gruvbox").setup({
 			contrast = "hard",
 		})
-
 		vim.cmd.colorscheme("gruvbox")
 	end,
 }
