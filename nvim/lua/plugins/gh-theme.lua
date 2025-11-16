9e1e661aa999116a29529cf85b2ac966884d9b8a
Albin Groen <albin.groen@gmail.com>
2025-11-16 21:54:37 +0100

    refacotr: update theming

--- nvim/lua/plugins/gh-theme.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"projekt0n/github-nvim-theme",
-	name = "github-theme",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		vim.cmd("colorscheme github_dark_colorblind")
-	end,
-}
